1f3d66dc644d5593892c79169bab8b5976f611b0
Yamil Asusta <hello@yamilasusta.com>
2017-11-11 14:14:16 -0400

    Initial position for sell

--- index.js
+++ index.js
@@ -2,13 +2,20 @@
 const monitor = require('./lib/monitor');
 const trade = require('./lib/trade');
 
-function dispatchAction(client, action, quantity, percent, cb) {
-  monitor(client, { percent, action }, (err, data) => {
+function dispatchAction(client, action, opts, cb) {
+  const quantity = opts.quantity;
+  const percent = opts.percent;
+
+  const monitorOpts = { percent, action, buyIn: quantity };
+  if (action === 'sell') {
+    monitorOpts.initialPrice = opts.initialPrice;
+  }
+  monitor(client, monitorOpts, (err, data) => {
     if (err) {
       return cb(err);
     }
     console.log(action, 'position at', data.price);
-    trade(client, { quantity, action }, (err) => {
+    trade(client, { quantity, action, price: data.price }, (err) => {
       if (err) {
         return cb(err);
       }
@@ -16,7 +23,7 @@ function dispatchAction(client, action, quantity, percent, cb) {
       // should come from trade fn
       const transaction = {};
       if (action === 'buy') {
-        transaction.acquired = quantity/data.price;
+        transaction.acquired = quantity / data.price;
       } else if (action === 'sell') {
         transaction.sold = quantity * data.price;
       }
@@ -28,12 +35,12 @@ function dispatchAction(client, action, quantity, percent, cb) {
 function run(client, fund=100, buyPercent=0.03, sellPercent=0.05) {
   const buyQuantity = fund / 2;
   fund -= buyQuantity;
-  dispatchAction(client, 'buy', buyQuantity, buyPercent, (err, position) => {
+  dispatchAction(client, 'buy', { percent: buyPercent, quantity: buyQuantity }, (err, position) => {
     if (err) {
       throw err;
     }
 
-    dispatchAction(client, 'sell', position.acquired, sellPercent, (err, receipt) => {
+    dispatchAction(client, 'sell', { percent: sellPercent, quantity: position.acquired }, (err, receipt) => {
       if (err) {
         throw err;
       }
