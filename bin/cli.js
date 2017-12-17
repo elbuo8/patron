46734a7ab8f57dcc08c97427d440c69d04ee93fe
Yamil Asusta <hello@yamilasusta.com>
2017-12-17 12:00:51 -0400

    Add administrative tooling

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,7 @@ const vorpal = require('vorpal')();
 
 const patron = require('../');
 const clientFactory = require('../lib/clientFactory');
+const accountOverview = require('../lib/accountOverview');
 
 vorpal
   .command('trade', 'trades in a continous loop in GDAX')
@@ -21,5 +22,37 @@ vorpal
     vorpal.log('Terminating loop execution...');
     process.exit(0);
   });
+
+vorpal
+  .command('orders', 'lists all orders for market')
+  .option('-m --market <market>')
+  .action((args) => {
+    const simulate = process.env.SIMULATE;
+    const client = clientFactory(args.options.market, simulate);
+    accountOverview.getOrders(client, (err, data) => {
+      if (err) {
+        this.log(err);
+        return;
+      }
+
+      this.log(data);
+    });
+  });
+
+vorpal
+  .command('accounts', 'lists all marketi accounts')
+  .action(() => {
+    const simulate = process.env.SIMULATE;
+    const client = clientFactory(null, simulate);
+    accountOverview.getOrders(client, (err, data) => {
+      if (err) {
+        this.log(err);
+        return;
+      }
+
+      this.log(data);
+    });
+  });
+
 
 vorpal.delimiter('GDAX $').show();
